45218dc3e53afeda023032942878cf747d9c8634
alyzha <74619386+alyzha@users.noreply.github.com>
2020-11-20 13:22:24 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -8,3 +8,11 @@ in_pack = int(input("How many cans are in your pack?: "))
 packs = int(input("How many packs do you have?: "))
 
 print("You have a total of {} cans.".format(in_pack * packs))
+
+# Volume of rectangular prism
+print("Find out the volume of a rectangular prism.")
+length = int(input("What is the length?: "))
+width = int(input("What is the width?: "))
+height = int(input("What is the height?: "))
+
+print("The volume of the rectangular prism is {} units cubed.".format(length * width * height))
