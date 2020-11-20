75659592405d29eaeaa042831856c2c3c7206843
alyzha <74619386+alyzha@users.noreply.github.com>
2020-11-20 13:12:00 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -2,3 +2,9 @@
 print("What is your favourite colour?")
 colour = input()
 print(f"{colour} ?! Oh why, that's my favourite colour too!")
+
+# Ask for number of cans
+in_pack = int(input("How many cans are in your pack?: "))
+packs = int(input("How many packs do you have?: "))
+
+print("You have a total of {} cans.".format(in_pack * packs))
